b846f838b6bd112b26dc2ea3334a493b28ca2e8f
Alex Barry <alex.barry@gmail.com>
2017-10-19 13:01:11 -0400

    Brought in babel and webpack so we can compile things down to good old js.

---
+++ config/webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require("webpack");
+var path = require("path");
+
+module.exports = {
+  devtool: "inline-cheap-source-map",
+  target: "node",
+
+  externals: {
+    "enquirer": "commonjs enquirer",
+    "prompt-choices": "commonjs prompt-choices",
+    "ansi-colors": "commonjs ansi-colors"
+  },
+
+  entry: path.resolve(__dirname, "..", "src", "index.js"),
+
+  module: {
+    rules: [
+      { test: /.js$/, loaders: ["babel-loader"] }
+    ]
+  },
+
+  output: {
+    path: path.resolve(__dirname, ".."),
+    filename: "gitpair.js"
+  },
+
+  plugins: [
+    new webpack.IgnorePlugin(/\.(css|less|scss|sass)$/)
+  ]
+};
