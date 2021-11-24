6e807f9df350370291a0dd04bc6311b1593e7dc5
Benoit d'Oncieu <bdoncieu@gmail.com>
2021-11-24 11:30:26 +0000

    test: fix lint config and tests

--- src/utils/__tests__/strip-co-authoring.test.js
+++ src/utils/__tests__/strip-co-authoring.test.js
@@ -4,7 +4,7 @@ it('strips a co-authored-by trailer message', () => {
   const commitMsg = `Commit message 1
 
 Co-authored-by: marry@peterpaulandmarry.com`
-  expect(stripCoAuthorship(commitMsg)).toEqual('Commit message 1')
+  expect(stripCoAuthorship(commitMsg)).toBe('Commit message 1')
 })
 
 it('strips multiple co-authored-by trailer messages', () => {
@@ -12,7 +12,7 @@ it('strips multiple co-authored-by trailer messages', () => {
 
 Co-authored-by: marry@peterpaulandmarry.com
 Co-authored-by: paul@peterpaulandmarry.com`
-  expect(stripCoAuthorship(commitMsg)).toEqual('Commit message 2')
+  expect(stripCoAuthorship(commitMsg)).toBe('Commit message 2')
 })
 
 it('does not remove any other git trailer messages', () => {
@@ -20,7 +20,7 @@ it('does not remove any other git trailer messages', () => {
 
 Signed-off-by: Peter <peter@peterpaulandmarry.com>
 Acked-by: Marry <marry@peterpaulandmarry.com>`
-  expect(stripCoAuthorship(commitMsg)).toEqual(commitMsg)
+  expect(stripCoAuthorship(commitMsg)).toBe(commitMsg)
 })
 
 it('strips a co-authored-by trailer message in the middle of other trailers', () => {
@@ -29,7 +29,7 @@ it('strips a co-authored-by trailer message in the middle of other trailers', ()
 Signed-off-by: Peter <peter@peterpaulandmarry.com>
 Co-authored-by: paul@peterpaulandmarry.com
 Acked-by: Marry <marry@peterpaulandmarry.com>`
-  expect(stripCoAuthorship(commitMsg)).toEqual(`Commit message 4
+  expect(stripCoAuthorship(commitMsg)).toBe(`Commit message 4
 
 Signed-off-by: Peter <peter@peterpaulandmarry.com>
 Acked-by: Marry <marry@peterpaulandmarry.com>`)
